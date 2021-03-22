9ff09d81fc756e39d7bdf190c9e2706d6ee1058b
baguspahlefi2000 <baguspahlefi@student.telkomuniversity.ac.id>
2021-03-22 17:50:05 +0700

    initial commit

---
+++ app/src/main/java/org/d3if4064/dessertclicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if4064.dessertclicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
